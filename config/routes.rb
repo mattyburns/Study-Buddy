bbfaceac34583458e779bca6ef3290267eaecc1e
Matty Burns <matty@Mattys-MacBook-Air.local>
2017-11-22 20:52:54 -0500

    Added Github Omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
 
 root 'static_pages#index'
+
+get 'auth/:provider/callback', to: 'sessions#create'
+
+get 'logout', to: 'sessions#destroy'
+
 end
--- db/migrate/20171121002544_create_users.rb
+++ db/migrate/20171121002544_create_users.rb
@@ -3,12 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :provider
       t.string :uid
-      t.string :first_name
-      t.string :last_name
-      t.string :email
-      t.string :token
-      t.string :refresh_token
-      t.datetime :oauth_expires_at
+      t.string :name
 
       t.timestamps null: false
     end
