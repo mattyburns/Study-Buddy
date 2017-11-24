a5a2820ea3f076e2da3b502f43e934008c256cc7
Matty Burns <matty@Mattys-MacBook-Air.local>
2017-11-24 17:53:31 -0500

    Landing page/home page view

---
+++ app/javascript/react/src/components/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LandingPage =(props) =>{
+  return(
+    <div>
+      <h1>Please sign in to continue :)</h1>
+    </div>
+  )
+}
+
+
+export default LandingPage;
